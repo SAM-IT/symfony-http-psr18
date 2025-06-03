fa6b12ffb4e90801547ceac62441a1262ab1c83f
Sam Mousa <sam@mousa.nl>
2025-06-03 16:49:07 +0200

    feat: support sending custom headers

--- src/SymfonyHttpClient.php
+++ src/SymfonyHttpClient.php
@@ -55,6 +55,13 @@ final class SymfonyHttpClient implements SymfonyHttpClientInterface
             unset($options['user_data']);
         }
 
+        if (isset($options['headers'])) {
+            foreach($options['headers'] as $name => $value) {
+                $request = $request->withHeader($name, $value);
+            }
+            unset($options['headers']);
+        }
+
         if (!empty($options)) {
             throw new TransportException('Unsupported options where passed');
         }
